8042012390cf80151b883569b971b5c802ffb405
dimasabya <dimasabdulyasir9@gmail.com>
2026-08-08 16:42:57 +0700

    chore(backend): prepare API for Riway depelopment

--- apps/backend/src/server.ts
+++ apps/backend/src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app";
 
-const PORT = 3001;
+const PORT = Number(process.env.PORT) || 3001;
 
 app.listen(PORT, "0.0.0.0", () => {
   console.log(`API running on port ${PORT}`);
